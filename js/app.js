58828d19fd237a8f578ab9edc5a7432470a7d609
EpistemicPolymath <epistemicpolymath@gmail.com>
2019-09-26 16:37:07 -0400

    Setup JS for the categories in the form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("Hello from JS!");
+
+
+const linkCategory = document.querySelector("#linkCategory");
+
+let linkedCategories = [];
+
+linkCategory.addEventListener('keydown', (event) => {
+    if(event.keyCode === 188) { // Comma Keycode
+        event.preventDefault();
+        // Add that value to our array
+        linkedCategories.push(linkCategory.value);
+        // Then clear the input value
+        linkCategory.value = '';
+
+        // Display the updated categories
+        displayLinkCatgeories();
+    }
+});
+
+const displayLinkCatgeories = () => {
+    console.log("Display Link Categories");
+}
